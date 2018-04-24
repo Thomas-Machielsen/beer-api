6ea9232f7a0517d4ee406ec6bb05d27258afb794
Thomas Machielsen <tmachielsen@mirabeau.nl>
2018-04-24 22:51:54 +0200

    Fix(beerCtrl) fixed a typo

--- controllers/beers.js
+++ controllers/beers.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 const BeerSchema = require("../schemas/Beer");
 const RatingSchema = require("../schemas/Rating");
 const BeersService = require("../services/beers");
-const { STATUS_CODES } = require('../constants');
+const { STATUSCODES } = require('../constants');
 
 
 const Beers = new BeersService(Sequelize, BeerSchema, RatingSchema);
@@ -16,7 +16,7 @@ function getBeer(req, res) {
   Beers.getBeer(req)
     .then(results => res.json({ data: results }))
     .catch(err => {
-      res.status(STATUS_CODES.NOT_FOUND);
+      res.status(STATUSCODES.NOT_FOUND);
       res.json({ error: err });
     });
 }
@@ -30,7 +30,7 @@ function editBeer(req, res) {
   Beers.editBeer(req, res)
     .then(results => res.json(results))
     .catch(err => {
-      res.status(STATUS_CODES.NOT_FOUND);
+      res.status(STATUSCODES.NOT_FOUND);
       res.json(err);
     });
 }
@@ -44,7 +44,7 @@ function deleteBeer(req, res) {
   Beers.deleteBeer(req)
     .then(results => res.json(results))
     .catch(err => {
-      res.status(STATUS_CODES.NOT_FOUND);
+      res.status(STATUSCODES.NOT_FOUND);
       res.json(err);
     });
 }
@@ -58,7 +58,7 @@ function addBeer(req, res) {
   Beers.addBeer(req, res)
     .then(results => res.json(results))
     .catch(err => {
-      res.status(STATUS_CODES.NOT_FOUND);
+      res.status(STATUSCODES.NOT_FOUND);
       res.json(err);
     });
 }
