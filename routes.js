546e7c0296fda53e7f60f880227e8a6839a511a4
Thomas Machielsen <tmachielsen@mirabeau.nl>
2018-08-21 19:09:38 +0200

    fix(routes) adds a comma

--- routes.js
+++ routes.js
@@ -11,7 +11,7 @@ router.all('/*', (req, res, next) => {
   res.header("Access-Control-Allow-Origin", process.env.ALLOWEDCORS);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   // Set custom headers for CORS
-  res.header('Access-Control-Allow-Headers', process.env.ALLOWEDHEADERS, process.env.ALLOWEDHEADERS2);
+  res.header('Access-Control-Allow-Headers', `${process.env.ALLOWEDHEADERS}, ${process.env.ALLOWEDHEADERS2}`);
   if (req.method === 'OPTIONS') {
     return res.status(SUCCESS).end();
   }
