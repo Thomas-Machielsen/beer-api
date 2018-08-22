3df2164d77850029892951de8e86d9d8fa7146dc
Thomas Machielsen <tmachielsen@mirabeau.nl>
2018-08-22 16:21:14 +0200

    refactor(beers) makes the response more uniform

--- controllers/authenticationCtrl.js
+++ controllers/authenticationCtrl.js
@@ -3,19 +3,19 @@ const AuthenticateModel = require("../services/authentication");
 const getToken = (req, res) => {
   AuthenticateModel.getToken(req)
     .then(results => res.json({ data: results }))
-    .catch(err => res.json({ error: err }));
+    .catch(err => res.json(err));
 };
 
 const isLoggedin = (req, res, next) => {
   AuthenticateModel.isLoggedin(req, next)
     .then(() => next())
-    .catch(err => res.json({ error: err }));
+    .catch(err => res.json(err));
 };
 
 const isAdmin = (req, res, next) => {
   AuthenticateModel.isAdmin(req, next)
     .then(() => next())
-    .catch(err => res.json({ error: err }));
+    .catch(err => res.json(err));
 };
 
 module.exports = { getToken, isAdmin, isLoggedin };
--- controllers/beersCtrl.js
+++ controllers/beersCtrl.js
@@ -8,7 +8,7 @@ const Beers = new BeersService(Sequelize, BeerSchema, RatingSchema);
 
 const getBeer = (req, res) => {
   Beers.getBeer(req)
-    .then(results => res.json({ data: results }))
+    .then(results => res.json(results))
     .catch(err => {
       res.status(STATUSCODES.NOT_FOUND);
       res.json(err);
